30d1e8c41d5ba42b99aa85b53990c07d8a41c1f0
harishbisht29 <harishbisht29@gmail.com>
2019-11-06 08:24:25 +0530

    update __init__.py file

--- amfi_india_scraper_test/__init__.py
+++ amfi_india_scraper_test/__init__.py
@@ -1,2 +1,2 @@
-from amfi_india_scraper.Fund import Fund
-from amfi_india_scraper.MF_House import MF_House
+from amfi_india_scraper_test.Fund import Fund
+from amfi_india_scraper_test.MF_House import MF_House
